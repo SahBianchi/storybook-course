84d3e79e1bbbe1636265d8ad1e668549cd774c89
Samantha Bianchi <samantha.bianchi@onerpm.com>
2025-04-11 12:17:39 -0300

    Feat: Starting the project + Completion of mod1 and start of mod2

---
+++ libs/button/src/lib/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'sb-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class ButtonComponent {}
